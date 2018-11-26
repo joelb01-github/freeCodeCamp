086c175e065ab0e1ba2532ab11bba69b4cecafe1
jos <ebrosetup@gmail.com>
2018-11-26 15:28:07 +0000

    first commit file metadata app

---
+++ fileMetadataApp/routes/api/fileanalyse.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var multer  = require('multer')
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+/* POST home page. */
+router.post('/', upload.single('upfile'), function(req, res, next) {
+  // send the responde
+  res.json({
+    "filename": req.file.originalname,
+    "filesize": req.file.size
+  });
+});
+
+module.exports = router;
